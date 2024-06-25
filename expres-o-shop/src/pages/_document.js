768a2b472948a8efb8e8af166bd37280ebb8ff45
dancey-apple <dancey.apple@proton.me>
2024-06-25 16:14:50 -0500

    Style adjustments

--- expres-o-shop/src/pages/_document.js
+++ expres-o-shop/src/pages/_document.js
@@ -2,7 +2,9 @@ import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document() {
   return (
-    <Html lang="en">
+    <Html lang="en" style={{
+      backgroundColor: "#131728",
+    }}>
       <Head />
       <body>
         <Main />
--- expres-o-shop/src/pages/index.js
+++ expres-o-shop/src/pages/index.js
@@ -26,37 +26,62 @@ function addToCart(id) {
 if (menu.length === 0) return (<h1>Loading...</h1>);
 
   return (
-    <>
+    <main style ={{
+      display: "flex",
+      justifyContent: "center",
+      alignItems: "center",
+      flexDirection: "column"
+    }}>
       <img src='https://github.com/dancey-apple/imageLibrary/blob/main/express-0-coffee.png?raw=true'style={{
         display: "flex",
         justifyContent: "center",
         width: "200px",
       }}></img>
-      <h1>Welcome to Express-O Coffee!</h1>
-      <h4>Check out our Selection!</h4>
+      <div  style={{
+        color: "#e02880",
+        display: "flex",
+        flexDirection: "column",
+        justifyContent: "center",
+        alignItems: "center",
+      }}>
+        <h1>Welcome to Express-O Coffee!</h1>
+        <h4>Check out our Selection!</h4>
+      </div>
       <div style={{
         display: "flex", 
-        flexdirection: "row", 
-        flexwrap: "wrap", 
+        justifyContent: "center",
+        flexDirection: "row",
+        flexWrap: "wrap", 
         padding: "10px"
         }}>  
         {menu.map(item => {
           return (
             <div key={item.id} style={{
+              display: "flex",
+              color: "#e02880",
+              justifyContent: "space-between",
+              textAlign: "center",
+              flexDirection: "column",
               border: "1px solid cyan",
               borderRadius: "15px",
               margin: "10px",
               padding: "5px",
-              width: "200px",
-              height: "100px",
+              width: "300px",
             }}>
               <h3>{item.name}</h3>
               <p>{item.description}</p>
               <p>${item.price}</p>
-              <button onClick={() => addToCart(item.id)}>Add To Cart</button>
+              <button onClick={() => addToCart(item.id)} style={{
+                backgroundColor: "#e02880",
+                color: "#131728",
+                padding: "5px",
+                borderRadius: "15px",
+                border: "none",
+                cursor: "pointer",
+              }}>Add To Cart</button>
             </div>
           );
         })}
       </div>
-    </>
+    </main>
 )}
