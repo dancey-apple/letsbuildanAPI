b100bc4a0ae942c78bd1d55b584cd86d36ca7c63
dancey-apple <dancey.apple@proton.me>
2024-06-27 14:00:37 -0500

    cart page loads, has styling and button, button does nothing

--- expres-o-shop/src/pages/cart/index.js
+++ expres-o-shop/src/pages/cart/index.js
@@ -18,29 +18,64 @@ export default function Checkout() {
     }
 
     return(
-        <>
-            <h1>Cart</h1>
-            <h4>Here is our cart:</h4>
-            <div
-            style={{
+        <main>
+            <div id= "body" style ={{
                 display: "flex",
-                flexDirection: "row",
-                flexWrap: "wrap",
-                padding: "10px",
-            }}
-            >
-            {cartItems.cart.map((item) => {
-                return (
-                <div key={item.id}
-                    style={{}}
-                >
-                <h3>{item.id}</h3>
-                <p>{item.quantity}</p>
-                <button onClick={() => removeFromCart(item.id)}>Remove</button>
+                justifyContent: "center",
+                alignItems: "center",
+                flexDirection: "column"
+                }}>
+                <img src='https://github.com/dancey-apple/imageLibrary/blob/main/express-0-coffee.png?raw=true'style={{
+                    display: "flex",
+                    justifyContent: "center",
+                    width: "200px",
+                }}></img>
+                <div  style={{
+                    color: "#e02880",
+                    display: "flex",
+                    flexDirection: "column",
+                    justifyContent: "center",
+                    alignItems: "center",
+                    }}>
+                    <h1>CART</h1>
                 </div>
-                );
-            })}
+                <div
+                style={{
+                    display: "flex",
+                    flexDirection: "row",
+                    flexWrap: "wrap",
+                    padding: "10px",
+                }}
+                >
+                {cartItems.cart.map((item) => {
+                    return (
+                    <div key={item.id} style={{
+                        display: "flex",
+                        color: "#e02880",
+                        justifyContent: "space-between",
+                        textAlign: "center",
+                        flexDirection: "column",
+                        border: "1px solid cyan",
+                        borderRadius: "15px",
+                        margin: "10px",
+                        padding: "5px",
+                        width: "300px",
+                    }}>
+                    <h3>{item.id}</h3>
+                    <p>{item.quantity}</p>
+                    <button onClick={() => removeFromCart(item.id)} style={{
+                        backgroundColor: "#e02880",
+                        color: "#131728",
+                        padding: "5px",
+                        borderRadius: "15px",
+                        border: "none",
+                        cursor: "pointer",
+                    }}>Remove</button>
+                    </div>
+                    );
+                })}
+                </div>
             </div>
-        </>
+        </main>
     )
 }
